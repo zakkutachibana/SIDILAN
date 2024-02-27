69d44a2f22527109f7d9e86b6b0e76498aca2692
Ciptagusti <css.mil12@gmail.com>
2024-02-27 16:10:48 +0700

    Added books item design and DashboardFragment

--- app/src/main/java/com/zak/sidilan/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/zak/sidilan/ui/dashboard/DashboardFragment.kt
@@ -1,12 +1,15 @@
 package com.zak.sidilan.ui.dashboard
 
+import android.animation.LayoutTransition
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.zak.sidilan.R
 import com.zak.sidilan.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
@@ -19,19 +22,36 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
-
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        setView()
+        setViewModel()
+        setAction()
+        return binding.root
     }
 
+    private fun setView() {
+        binding.userCard.tvUserAction.text = "Surya Widjojo"
+        binding.userCard.tvUserName.text = "Administrator"
+    }
+
+    private fun setViewModel() {
+        val dashboardViewModel =
+            ViewModelProvider(this).get(DashboardViewModel::class.java)
+    }
+
+    private fun setAction() {
+        binding.btnExpand.setOnClickListener {
+            val expVis = if (binding.clExpand.visibility == View.GONE) {
+                binding.btnExpand.icon = ResourcesCompat.getDrawable(resources, R.drawable.ic_up, null)
+                View.VISIBLE
+            } else {
+                binding.btnExpand.icon = ResourcesCompat.getDrawable(resources, R.drawable.ic_down, null)
+                View.GONE
+            }
+            binding.clExpand.visibility = expVis
+        }
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
